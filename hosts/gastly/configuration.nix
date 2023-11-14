a3a595fb70d9759ea4835081d9d47545b8887765
Floris Stoica <floris@nusunt.eu>
2023-11-14 20:51:54 +0200

    add conf and hardware configs

---
+++ hosts/gastly/configuration.nix
@@ -0,0 +1,49 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running `nixos-help`).
+
+{ config, pkgs, lib, ... }:
+
+{
+  imports =
+    [ 
+      ./common/common.nix
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      <home-manager/nixos>
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "gastly"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enableFccUnlock = true;
+
+  environment.etc."ModemManager/fcc-unlock.d/8086:7560" = {
+    mode = "0777";
+    source = ./hosts/gastly/modem/8086-fcc_unlock.py;
+  };
+
+  services.udev.extraRules = with pkgs; ''
+SUBSYSTEM=="power_supply", ATTR{online}=="0", RUN+="${writeShellScriptBin "udev_on_bat" (builtins.readFile ./hosts/gastly/udev_on_bat.sh)}"
+SUBSYSTEM=="power_supply", ATTR{online}=="1", RUN+="${writeShellScriptBin "udev_on_ac" (builtins.readFile ./hosts/gastly/udev_on_ac.sh)}"
+'';
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
+
