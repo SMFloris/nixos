4bc1a44cf2401c629ce176ea15262da2b4d7245c
Floris Stoica <floris@nusunt.eu>
2024-04-05 16:20:01 +0300

    fix astronvim config path

--- common/flow.nix
+++ common/flow.nix
@@ -70,8 +70,8 @@ in
   home.file.".config/nvim" = {
     recursive = true;
     source = builtins.fetchGit {
-		  # url = "https://github.com/SMFloris/astronvim-config";
-		  url = "/home/flow/Projects/astronvim-config";
+		  url = "https://github.com/SMFloris/astronvim-config";
+		  # url = "/home/flow/Projects/astronvim-config";
 		  ref = "main";
 	  };
   };
