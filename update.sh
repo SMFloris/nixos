2e2f26c30e2bed5eeb0068ff23a23ba9f8f9db66
Floris Stoica <floris@nusunt.eu>
2024-06-22 09:08:30 +0300

    added update script for easy update

---
+++ update.sh
@@ -0,0 +1,37 @@
+#/usr/bin/env bash
+
+RELEASE=24.05
+
+# List of inputs to search for in the channel's URL
+inputs=("home-manager" "nixos" "nixos-unstable")
+# Get the channel's URL
+declare -A urls
+urls["home-manager"]="https://github.com/nix-community/home-manager/archive/release-${RELEASE}.tar.gz"
+urls["nixos"]="https://nixos.org/channels/nixos-${RELEASE}"
+urls["nixos-unstable"]="https://nixos.org/channels/nixos-unstable"
+# Iterate over the list of inputs
+for input in "${inputs[@]}"
+do
+    channel_url=$(nix-channel --list | grep -o "$input https://.*")
+    # Check if the channel's URL contains the current input
+    if [[ $channel_url == *"$input"* ]]; then
+        echo "The channel's URL contains '$input'"
+        # In bash write some code that gets second column of channel_url string (split by whitespace) and checks it against a key value array that has the key 'input'
+        channel_url_split=($channel_url)
+        if [[ ${channel_url_split[1]} == ${urls[$input]} ]]; then
+            echo "Channel is ok"
+        else
+            nix-channel --remove ${input}
+            nix-channel --add ${input} ${urls[$input]}
+            echo "Nix channel replaced with latest url"
+        fi
+    else
+        echo "The channel's URL does not contain '$input'"
+        nix-channel --add ${input} ${urls[$input]}
+        echo "Added the correct url"
+    fi
+done
+
+echo "Updating channels"
+nix-channel --update
+
